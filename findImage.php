a7a8b2002228d28ba37f59a2f55f048845781ee0
Ravindrage <gehlotravindra@gmail.com>
2020-11-16 17:33:58 +0530

    Add Commant

---
+++ findImage.php
@@ -0,0 +1,78 @@
+<?php
+
+
+include 'db/dbsetup.php'; 
+global $con;
+$zip = new ZipArchive();
+
+
+
+$sqlBooks = mysqli_query($con,"SELECT * FROM books");
+//$rowBooks = mysqli_fetch($sqlBooks);
+$i=0;
+
+while ($rowServices = mysqli_fetch_array($sqlBooks)){
+    
+	$i++;
+	//print_r($rowServices);
+	echo '<br>';
+	
+if($i==20) { exit; }
+
+echo $filename = "uploads/ebook/50000_ebooks/".$rowServices['title'].".epub";
+
+
+if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {
+    exit("cannot open <$filename>\n");
+}else
+{   mkdir("uploads/ebook/Html/".$rowServices['title'],777);
+
+	$zip->extractTo("uploads/ebook/Html/".$rowServices['title']);
+	$zip->close();
+	
+		
+}
+
+
+    $dirname = "uploads/ebook/Html/".$rowServices['title']."/OEBPS/Images";
+    $filenames = glob("$dirname/*cover*.jpg");
+
+    foreach ($filenames as $filename)
+    {
+        
+		
+		$ext = pathinfo($filename, PATHINFO_EXTENSION);
+		//echo move_uploaded_file($filename,"upload/ebook/coverImage/".$rowServices['id'].'.'.$ext);
+		echo $filename;
+		echo "upload/ebook/coverImage/".$rowServices['id'].'.'.$ext ;
+		if( copy($filename,"upload/ebook/coverImage/"))
+		{
+		rename("uploads/ebook/coverImage/".$filename,"upload/ebook/coverImage/".$rowServices['title'].'.'.$ext);
+		echo "Update books set image='".$rowServices['title'].$ext."' where id='".$rowServices['id']."'" ;
+		mysqli_query($con,"Update books set image='".$rowServices['title'].$ext."' where id='".$rowServices['id']."' ");
+		}
+    }
+	
+	$dirname = "uploads/ebook/Html/".$rowServices['title']."/OEBPS";
+    $filenames = glob("$dirname/*cover*.jpg");
+
+    foreach ($filenames as $filename)
+    {
+       
+		$ext = pathinfo($filename, PATHINFO_EXTENSION);
+		echo '<br>';
+		echo $filename; echo '------------';
+		echo "upload/ebook/coverImage/".$rowServices['id'].'.'.$ext ;
+		echo '<br>';
+		if( copy($filename,"upload/ebook/coverImage/"))
+		{
+		rename("uploads/ebook/coverImage/".$filename,"upload/ebook/coverImage/".$rowServices['title'].'.'.$ext);
+		echo "Update books set image='".$rowServices['title'].$ext."' where id='".$rowServices['id']."'" ;
+		mysqli_query($con,"Update books set image='".$rowServices['title'].$ext."' where id='".$rowServices['id']."' ");
+		}
+    }
+	
+	
+
+}
+?>
